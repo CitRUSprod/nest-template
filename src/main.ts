62f34f59587b40ad6e5f01273b0979772a222f14
CitRUS <citrus4009@gmail.com>
2022-02-10 00:18:34 +0300

    refactor: Refactor file architecture

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from "@nestjs/core"
+import { Logger } from "@nestjs/common"
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify"
-import { AppModule } from "./app.module"
+import { AppModule } from "$/modules/app.module"
 
 const port = 3000
+const logger = new Logger("Main")
 
 async function start() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
     await app.listen(port)
-    console.log(`http://localhost:${port}`)
+    logger.log(`Running on http://localhost:${port}`)
 }
 
 start()
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
-import { PostsModule } from "$/posts/posts.module"
+import { PostsModule } from "$/modules/posts/posts.module"
 
 @Module({
     imports: [
